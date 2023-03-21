44cb34ee8376d61252770d0735d9b7a198aee516
Vih0 <araujovitoris1.4@gmail.com>
2023-03-21 08:06:29 -0300

    testes formulario

--- src/components/Form.test.jsx
+++ src/components/Form.test.jsx
@@ -1,29 +1,28 @@
-import {render, screen, waitFor , } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import { describe, it, vi } from "vitest";
 import { Form } from "./Form";
 
 describe("Form Component", () => {
-    it("should render Form", () => {
-        render(<Form />);
-        const input = screen.getByTitle("Descreva sua Ideia");
-        const button = screen.getByRole('button')
-        expect(input).toBeInTheDocument();
-        expect(button).toBeInTheDocument();
-        });
-    it("should call submit function when button is clicked", async () => {
-            const event = userEvent.setup()
-            const sbmt = vi.fn()
-            render(<Form onSubmit={sbmt}/>)
-            const input =screen.getByTestId('form-field')
-            const btn = screen.getByTestId('form-btn')
-            input.focus()
-            event.type(input, 'teste')
-            console.log(btn)
-            await waitFor(async () => await event.click(btn))
-            
-            expect(sbmt).toHaveBeenCalledTimes(1)
-       });
-})
+  it("should render Form", () => {
+    render(<Form />);
+    const input = screen.getByTitle("Descreva sua Ideia");
+    const button = screen.getByRole("button");
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+  it("should call submit function when button is clicked", async () => {
+    const event = userEvent.setup();
+    const onSubmit = vi.fn();
 
+    render(<Form onSubmit={onSubmit} />);
+    const input = screen.getByTestId("form-field");
+    const btn = screen.getByTestId("form-btn");
+    input.focus();
+    await event.type(input, "ideia 1");
+    await event.click(btn);
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+    expect(input).toHaveValue("ideia 1");
+  });
+});
